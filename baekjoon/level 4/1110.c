1babbbd31482ffdd0883de10c7281fc0c3b5a25a
Pleiades <31816086+PeltX@users.noreply.github.com>
2021-04-23 17:20:27 +0900

    Update 1110.c

--- baekjoon/level 4/1110.c
+++ baekjoon/level 4/1110.c
@@ -2,17 +2,17 @@
 #include<stdio.h>
 int main(void)
 {
-	int N, ten, one, c, d, count=0;
+	int N, ten, one, newone, newN, count=0;
 
 	scanf("%d", &N);
 	ten = N / 10; one = N % 10;
 	while (1)
 	{
-		c = (ten + one) % 10; 
-		d = one * 10 + c;
-		ten = d / 10; one = d % 10;
+		newone = (ten + one) % 10; 
+		newN = one * 10 + newone;
+		ten = newN / 10; one = newN % 10;
 		count++;
-		if (N == d) break;
+		if (N == newN) break;
 	}
 	printf("%d", count);
 }
