5b0db17fc4f3a77e0c5befe91fddf291a668846e
PELT <31816086+PeltX@users.noreply.github.com>
2024-06-02 07:38:02 +0900

    Create 10172.c

---
+++ baekjoon/IO & elementary arithmetic  /10172.c
@@ -0,0 +1,11 @@
+//10172, 강아지 20240602
+#include<stdio.h>
+
+int main()
+{
+	printf("|\\_/|\n"); //\ 출력에 유의
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|");
+}
