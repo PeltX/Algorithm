3eae7e9bfd787a41d70a968c75f8f9bd5599676f
PELT <31816086+PeltX@users.noreply.github.com>
2024-06-03 07:02:31 +0900

    Create 1330.c

---
+++ baekjoon/Conditional /1330.c
@@ -0,0 +1,13 @@
+//1330, 두 수 비교하기 20240603
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	if (a > b) printf(">");
+	if (a < b) printf("<");
+	if (a == b) printf("==");
+
+}
