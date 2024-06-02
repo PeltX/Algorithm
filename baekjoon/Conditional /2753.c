281a760fa6f563099b6ba537826ce8e2c65bd718
PELT <31816086+PeltX@users.noreply.github.com>
2024-06-03 07:09:47 +0900

    Create 2753.c

---
+++ baekjoon/Conditional /2753.c
@@ -0,0 +1,14 @@
+//2753, 윤년 20240603
+#include<stdio.h>
+
+int main()
+{
+	int year;
+
+	scanf("%d", &year);
+
+	if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0))
+		printf("1");
+	else
+		printf("0");
+}
