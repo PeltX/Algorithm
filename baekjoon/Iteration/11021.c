a11c00493d21c9324ebbb2bfb059b8a67aef9fbe
PELT <31816086+PeltX@users.noreply.github.com>
2024-06-05 08:11:59 +0900

    Create 11021.c

---
+++ baekjoon/Iteration/11021.c
@@ -0,0 +1,12 @@
+//11021, A+B-7 20240605
+#include<stdio.h>
+int main(void)
+{
+	int a, b, i, num;
+	scanf("%d", &num);
+	for (i = 1; i <= num; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n",i, a + b);
+	}
+}
