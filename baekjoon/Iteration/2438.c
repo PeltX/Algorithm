310cb0668ef4368541eb4d78cf7be858136b7a48
PELT <31816086+PeltX@users.noreply.github.com>
2024-06-06 13:18:23 +0900

    Create 2438.c

---
+++ baekjoon/Iteration/2438.c
@@ -0,0 +1,17 @@
+//2438, 별 찍기-1 20240606
+#include<stdio.h>
+int main(void)
+{
+	int i, j, n;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
