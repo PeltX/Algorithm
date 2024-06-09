e60bfda51124a5f6ad5d18b7350d332075b6305c
PeltX <zero_knight_@naver.com>
2024-06-09 22:08:29 +0900

    20240609

---
+++ baekjoon/one-dimensional array/1546.c
@@ -0,0 +1,24 @@
+//1546, Æò±Õ 20240609
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int N, i, M=0, sum=0;
+	int* score;
+	double * nscore;
+	double nsum=0;
+	double avg;
+	scanf("%d ", &N);
+	score = (int*)malloc(sizeof(int) * N);
+
+	for (i = 0; i < N; i++)
+	{
+		scanf("%d", &score[i]);
+		M = (score[i] > M) ? score[i] : M; // ÃÖ´ñ°ª
+		sum += score[i];
+	}
+	nsum = (double)sum / M * 100;
+	avg = nsum / N;
+	printf("%lf",avg);
+}
