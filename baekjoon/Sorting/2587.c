593cbab3dd721c7f1f2c1ed9ff6af4ae239d3e83
PeltX <zero_knight_@naver.com>
2024-07-09 09:39:46 +0900

    20240709

--- baekjoon/Sorting/2587.c
+++ baekjoon/Sorting/2587.c
@@ -2,13 +2,16 @@
 #include<stdio.h>
 int main()
 {
-	int arr[5], i, j, tmp, sum=0, avg, med;
+	int arr[5], i, j, tmp, sum=0, avg;
 
 	for (i = 0; i < 5; i++)
+	{
 		scanf("%d", &arr[i]);
+		sum+=arr[i];
+	}
 
 	//버블 정렬
-	for (i = 0; i < 5; i++) //배열을 순회하는 횟수는 4회(배열값을 합해주기 위해 범위를 1 늘렸다. 이 경우 4-i=0이므로 내부 for 문은 실행되지 않는다.
+	for (i = 0; i < 4; i++) //배열을 순회하는 총 횟수는 4회
 	{
 		for (j = 0; j < 4 - i; j++) //최초 순회 시 4회 비교. 1회 순회할 때마다 다음 순회 시 비교할 배열 개수 -1
 		{
@@ -19,10 +22,8 @@ int main()
 				arr[j + 1] = tmp;
 			}
 		}
-		sum += arr[i];
 	}
 
 	avg = sum / 5;
 	printf("%d\n%d", avg, arr[2]);
-	
 }
